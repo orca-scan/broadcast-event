f69aefee5b946ce58354de9f9939e99a9ed51482
John Doherty <john@orcascan.com>
2025-02-08 18:53:57 +0100

    Update broadcast-event.js

--- src/broadcast-event.js
+++ src/broadcast-event.js
@@ -12,7 +12,7 @@
     if (typeof window.EventTarget !== 'function') throw new Error('missing EventTarget polyfill');
 
     var sender = window.location.href;
-    var eventIdCache = [];
+    var originId = stringHash(sender + ':' + performance.now() + ':' + Math.random());
     var debugging = true; // uncomment to console log sequence
 
     /**
@@ -31,35 +31,40 @@
      * @param {boolean} [debug] - console log flow if true (default false)
      * @returns {void}
      */
-    function broadcastEvent(eventName, eventData, debug) {
+    function broadcastEvent(eventName, eventData, eventOriginId) {
 
         eventName = String(eventName || '') || '';
-        eventData = eventData || {};
-        debugging = (debug === true);
+        // debugging = (debug === true);
 
         if (eventName.indexOf(':') === -1) throw new Error('eventName must be namespaced with :');
 
-        // exit if we already sent this
-        if (alreadyBroadcast(eventData)) return;
+        // dont rebroadcast our own events
+        if (eventOriginId === originId) {
+            log('suppressed:', eventName);
+            return;
+        }
 
         var data = {
+            originId: eventOriginId || originId,
             type: eventName,
             detail: eventData,
-            sender: sender,
-            timestamp: Date.now()
+            eventId: stringHash(eventName + ':' + performance.now() + ':' + Math.random())
         };
 
-        // dispatch locally
         log('fired:', data);
+
+        // dispatch locally
         window.dispatchEvent(new CustomEvent(eventName, eventData));
 
-        // send to other frames
+        // it sometimes sends twice
+        // problem is, if another scripts injects a frame, we send data to it :o
+
+        // we're in an iframe, send to parent
         if (window.top !== window) {
-            // we're in an iframe, send to parent
             sendEvent(window.top, data);
         }
         else {
-            // we're topmost page, send to all child frames
+            // send to all child frames
             for (var i = 0, l = window.frames.length; i < l; i++) {
                 sendEvent(window.frames[i], data);
             }
@@ -78,25 +83,6 @@
     }
 
     /**
-     * Checks if this event has already been broadcast by us
-     * @param {object} eventData - event data
-     * @returns {boolean} true if we've already send this, otherwise false
-     */
-    function alreadyBroadcast(eventData) {
-
-        if (!eventData) throw new Error('Invalid payload param');
-
-        var eventIds = eventData.eventIds || [];
-        
-        // check if this event id was issued by us
-        var matchFound = eventIdCache.some(function(guid) {
-            return eventIds.indexOf(guid) !== -1;
-        });
-
-        return matchFound;
-    }
-
-    /**
      * Send an event to another window using postMessage
      * @param {Window} targetWindow - window to send message to
      * @param {object} payload - data to send
@@ -106,24 +92,6 @@
 
         // dont send to self
         if (targetWindow === window) return;
-
-        // exit if we already sent this
-        if (alreadyBroadcast(payload.detail)) return;
-    
-        // generate a unique event ID to prevent rebroadcast loops
-        var eventId = stringHash(sender + ':' + payload.type + ':' + performance.now() + ':' + Math.random());
-        
-        // add new event ID (ensures if we see this event again, we dont rebroadcast)
-        payload.detail.eventIds = payload.detail.eventIds || [];
-        payload.detail.eventIds.push(eventId);
-
-        // store this event ID in cache
-        eventIdCache.push(eventId);
-    
-        // dont let event cache grow beyond 500 items
-        if (eventIdCache.length > 1000) {
-            eventIdCache.splice(0, eventIdCache.length - 1000);
-        }
 
         try {
             log('sent:', payload);
@@ -156,14 +124,16 @@
      */
     window.addEventListener('message', function(event) {
 
+        if (event.source === window) return;
+
         var _broadcast = event.data && event.data._broadcast;
 
-        if (_broadcast && typeof _broadcast.type === 'string' && _broadcast.detail) {
+        if (_broadcast && typeof _broadcast.type === 'string') {
 
             log('received:', _broadcast);
 
             // share with other frames
-            broadcastEvent(_broadcast.type, _broadcast.detail);
+            broadcastEvent(_broadcast.type, _broadcast.detail, _broadcast.originId);
         }
     });
 
